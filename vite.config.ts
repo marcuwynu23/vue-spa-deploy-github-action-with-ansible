9468900369d42a7ae02f84ae1e7b1aa5e749e77a
Mark Wayne Menorca <marcuwynu23@gmail.com>
2025-03-20 12:24:31 +0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/vue-spa-deploy-github-action-with-ansible/",
   plugins: [vue()],
   build: {
     outDir: "build",
-  }
-})
+  },
+});
